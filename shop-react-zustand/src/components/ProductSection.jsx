2a9521309597ed5e6b3c6de61f784aefae27978a
sandarX <sandar111301@gmail.com>
2024-08-18 20:30:57 +0630

    Update: Category filter to show related products

--- shop-react-zustand/src/components/ProductSection.jsx
+++ shop-react-zustand/src/components/ProductSection.jsx
@@ -3,19 +3,28 @@ import React from "react";
 import { Container } from "./Container";
 import ProductCard from "./ProductCard";
 import useProductStore from "../store/useProductStore";
+import useCategoryStore from "../store/useCategoryStore";
 
 const ProductSection = () => {
-  
   const { products } = useProductStore();
+  const { categories } = useCategoryStore();
+
+  const currentCategory = categories.find((el) => el.isActive === true);
 
   return (
     <section className="px-5 py-3">
       <Container>
         <h1 className="text-lg text-slate-600 mb-5">Available Products</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
+          {products
+            .filter((el) =>
+              currentCategory.name === "All"
+                ? el
+                : el.category === currentCategory.name
+            )
+            .map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
         </div>
       </Container>
     </section>
